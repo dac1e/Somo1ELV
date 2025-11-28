21225fe889dae5ca875f492e56a9f064325c896a
dac1e <wolfgang@die-schmieders.de>
2025-11-28 18:23:01 +0100

    Fixed arduino-lint issues.

---
+++ examples/PrintSensorValues/sloeber.ino.cpp
@@ -0,0 +1,18 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2025-11-28 18:15:00
+
+#include "Arduino.h"
+#include <Arduino.h>
+#include "Somo1ELV.h"
+
+static void setupSomo1() ;
+void setup() ;
+void loop() ;
+
+
+#include "PrintSensorValues.ino"
+
+#endif
